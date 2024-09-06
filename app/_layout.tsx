bfee5c979ad7c54d3a785d77e702cbffc9bfdf7e
GustavoZeglan <luizgustavozeglan1@gmail.com>
2024-09-06 13:50:44 -0300

    removing some tabs files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { SQLiteProvider } from 'expo-sqlite';
+import { initDatabase } from '@/database/initiDatabase';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,14 +30,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown: false}}  />
-        {/*<Stack.Screen name="(tabs)" options={{ headerShown: false }} />*/}
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen name="game_center" options={{headerShown: false}}/>
-        <Stack.Screen name="monkeyDetails" options={{headerShown: false}}/>
-      </Stack>
-    </ThemeProvider>
+    <SQLiteProvider databaseName='chimp.db' onInit={initDatabase}>
+        <Slot/>
+    </SQLiteProvider>
   );
 }
