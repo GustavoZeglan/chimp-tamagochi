4fd66c9e4d8f5203cfc8090ae20c36d2ce6cb36b
Pedro Vinicius <pvinicius2005@hotmail.com>
2024-09-08 10:25:23 -0300

    criando a tela do jogo pedrapapeltesoura

---
+++ app/rockPaperScissorGame.tsx
@@ -0,0 +1,80 @@
+import {Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {useState} from "react";
+
+
+const rockPaperScissorGame =() => {
+
+    const [resultado,setResultado] = useState<string>();
+    const [randomChoice,setRandomChoice] = useState<string>();
+
+    const options = ["Rock","Paper","Scissor"];
+
+    function getRandomInt() {
+        return Math.floor(Math.random() * options.length);
+    }
+
+    const play = (playerChoice : string) => {
+        const randomChoice = options[getRandomInt()]
+        setRandomChoice(randomChoice);
+
+        // Gustavo guanabara me perdoe por esse codigo
+        if (playerChoice === "rock" && randomChoice === "Paper") {
+            setResultado("Você Perdeu :(");
+        } else if (playerChoice === "rock" && randomChoice === "Scissor") {
+            setResultado("Você Ganhou :)");
+        } else if (playerChoice === "rock" && randomChoice === "Rock") {
+            setResultado("Empate!!!");
+        } else if (playerChoice === "paper" && randomChoice === "Rock") {
+            setResultado("Você Ganhou :)");
+        } else if (playerChoice === "paper" && randomChoice === "Scissor") {
+            setResultado("Você Perdeu :(");
+        } else if (playerChoice === "paper" && randomChoice === "Paper") {
+            setResultado("Empate!!!");
+        } else if (playerChoice === "scissor" && randomChoice === "Rock") {
+            setResultado("Você Perdeu :(");
+        } else if (playerChoice === "scissor" && randomChoice === "Paper") {
+            setResultado("Você Ganhou :)");
+        } else if (playerChoice === "scissor" && randomChoice === "Scissor") {
+            setResultado("Empate!!!");
+        }
+    }
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Pedra Papel Tesoura</Text>
+            <View style={styles.choiceContainer}>
+
+                <TouchableOpacity onPress={()=>play("rock")}>
+                    <Image source={require("../assets/images/rockpaperscissorIcons/rock.png")}></Image>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>play("paper")}>
+                    <Image source={require("../assets/images/rockpaperscissorIcons/paper.png")}></Image>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>play("scissor")}>
+                 <Image source={require("../assets/images/rockpaperscissorIcons/scissor.png")}></Image>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.resultContainer}>
+                <Text style={styles.choiceText}>{randomChoice? "O Macaquinho Escolheu:" : ""}</Text>
+                <Text style={styles.randomChoiceText}>{randomChoice}</Text>
+                <Text style={resultado === "Você Ganhou :)" ? styles.resultTextWinner : styles.resultTextLoser}>{resultado}</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1,backgroundColor:"#262b44",alignItems:"center",padding:48},
+    title : {fontSize:24,color:"white",fontFamily:"PressStart2P",textAlign:"center"},
+    choiceContainer:{display:"flex",flexDirection:"row",gap:24,margin:48},
+    choiceText:{fontSize:18,color:"white",fontFamily:"PressStart2P",textAlign:"center"},
+    randomChoiceText:{fontSize:14,color:"white",fontFamily:"PressStart2P"},
+    resultContainer: {display:"flex",gap:8,alignItems:"center"},
+    resultTextLoser: {fontSize:14,fontFamily:"PressStart2P",color:"red"},
+    resultTextWinner: {fontSize:14,fontFamily:"PressStart2P",color:"green"}
+})
+
+export default rockPaperScissorGame;
