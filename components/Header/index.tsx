36c5ddb5ea33c183640d1ed32aca00432b8825fc
Pedro Vinicius <pvinicius2005@hotmail.com>
2024-09-10 21:53:52 -0300

    criando navegacao para o botao de inicio

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,21 +1,29 @@
-import {Image, Text, View} from "react-native";
+import {Image, Text, TouchableOpacity, View} from "react-native";
 import {headerIconStyle, styles, headerTextStyle} from "@/components/Header/styles";
 
 // @ts-ignore
 import openDoorIcon from "../../assets/images/openDoorIcon.png";
+import {Href, useRouter} from "expo-router";
 
 interface HeaderProps {
     title: string;
 }
 
 const Header = ({title} : HeaderProps) => {
+
+    const router = useRouter();
+    const handleNavigate = (route: Href) => {
+        router.navigate(route);
+    }
+
+
     return (
         <View style={styles.container}>
             <Text style={headerTextStyle.primaryText}>{title}</Text>
-            <View style={headerIconStyle.container}>
+            <TouchableOpacity style={headerIconStyle.container} onPress={()=>handleNavigate("/")}>
                 <Image source={openDoorIcon}></Image>
                 <Text style={headerIconStyle.text}>Inicio</Text>
-            </View>
+            </TouchableOpacity>
         </View>
     )
 }
